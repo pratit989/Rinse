37073202dbbab163fa219d9c4e863c728d77d312
Pratit Shivaji Todkar <Pratittodkar@gmail.com>
2022-02-19 07:42:24 +0530

    Deleted confirm_delivery_widget.dart

--- lib/ongoing_delivered_to_customer/ongoing_delivered_to_customer_widget.dart
+++ lib/ongoing_delivered_to_customer/ongoing_delivered_to_customer_widget.dart
@@ -1,8 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-import '../auth/auth_util.dart';
 import '../backend/schema/orders_record.dart';
-import '../confirm_delivery/confirm_delivery_widget.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
